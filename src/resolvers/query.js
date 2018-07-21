4ff9ed39c4d79c3f6b77478c4b9cac1e29d523f5
Damien Bidaud <damien.bidaud@eurostar.com>
2018-07-21 20:29:16 +0100

    add author query

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,13 +1,10 @@
 const jwt = require("jsonwebtoken");
 const { getActivity } = require("../utils");
 
-const activities = (_, args, context, info) => {
-  return context.prisma.query.activities({}, info);
-};
-
 const validateAuth = (_, args, context, info) => {
   const { token } = args;
   try {
+    console.log(token)
     var decoded = jwt.verify(token, process.env.APP_SECRET);
     return decoded.authorId;
   } catch (err) {
@@ -22,9 +19,24 @@ const locations = (_, args, context, info) => {
   return context.prisma.query.locations({}, info);
 }
 
+const author = (_, args, context, info) => {
+  const authorId = validateAuth(_, args, context, info);
+  if(!!authorId) {
+    return context.prisma.query.author(
+      {
+        where: {
+          id: authorId
+        }
+      },
+      info
+    );
+  }
+  return null;
+}
+
 module.exports = {
-  activities,
   validateAuth,
   activity,
-  locations
+  locations,
+  author
 };
